ddc7a7897b56ae325c1803ddb58160936194774e
RR-LIBRARY <rr.digital.library@gmail.com>
2026-01-22 20:21:15 +0000

    Fix tabs component and ensure utils exist

--- src/lib/utils.ts
+++ src/lib/utils.ts
@@ -1,7 +1,6 @@
 import { type ClassValue, clsx } from "clsx";
 import { twMerge } from "tailwind-merge";
 
-// Yeh function multiple classes ko safely merge karta hai (Tailwind conflicts hatata hai)
 export function cn(...inputs: ClassValue[]) {
   return twMerge(clsx(inputs));
 }
